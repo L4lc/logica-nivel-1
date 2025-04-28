592d5ce060003d946df2470d71825e4fb5f82230
L4lc <l4ladl1995@gmail.com>
2025-04-28 19:15:02 -0300

    Create ex-02.py

---
+++ ex-02.py
@@ -0,0 +1,4 @@
+# 2. Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+print("Lavinia")
+print("Rio de Janeiro")
+print("Parque Analandia")
