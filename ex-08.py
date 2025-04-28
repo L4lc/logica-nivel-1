276c807210cb454154c77da1169dd6fe63abc5b5
L4lc <l4ladl1995@gmail.com>
2025-04-28 20:33:11 -0300

    Create ex-08.py

---
+++ ex-08.py
@@ -0,0 +1,5 @@
+. Implemente um programa que na primeira linha faça um print perguntando o nome do usuário e, na linha 2, chame a função input() para capturar o valor do teclado e armazenar na memória do computador. No fim, de boas vindas ao usuário com o seu nome aparecendo na tela.#Implemente um programa que na primeira linha faça um print perguntando o nome do usuário e, na linha 2, chame a função input() para capturar o valor do teclado e armazenar na memória do computador. No fim, de boas vindas ao usuário com o seu nome aparecendo na tela.
+
+print("Nome de Usuario")
+nome= input()
+print("Boas Vidas!",nome)
