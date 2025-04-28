f4ff05c9bcea87e884d325297ea610d682eeba27
L4lc <l4ladl1995@gmail.com>
2025-04-28 20:08:06 -0300

    Create ex-07.py

---
+++ ex-07.py
@@ -0,0 +1,11 @@
+# Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+#Enriqueça o desenho com outros caracteres, simulando enfeites.
+print(". .==x=")
+print("- .-XXX.")
+print(" .~XXXXX~")
+print(". XXXXXXX~")
+print("~XXXXXXXXX")
+print("XXXXXXXXXXX")
+print("    XX")
+print("    xx")
+print("   xxxx")
